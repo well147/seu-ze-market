74b1918959b0b4e30b528bb481a9f4f5bb120e47
well147 <wellerson_drumond@hotmail.com>
2020-11-27 19:16:37 -0300

    Add product images

---
+++ src/pages/products.tsx
@@ -0,0 +1,7 @@
+import { Container } from 'next/app';
+
+const Products: React.FC = () => {
+  return <Container />;
+};
+
+export default Products;
---
+++ src/styles/pages/products.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 1fr);
+  grid-template-rows: repeat(auto-fill, 1fr);
+`;
