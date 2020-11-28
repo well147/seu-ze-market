235a95eb5f6daf7065811f76c38c87301bb04f7e
well147 <wellerson_drumond@hotmail.com>
2020-11-27 21:02:00 -0300

    feat: add additional products, delete unused product images

--- src/fixtures/products.ts
+++ src/fixtures/products.ts
@@ -1,7 +1,7 @@
 interface Product {
   name: string;
   description: string;
-  punchline: string;
+  punchline?: string;
   price: number;
   image: string;
 }
@@ -18,7 +18,7 @@ const products: Product[] = [
   {
     name: 'Beterraba',
     description: 'Beterraba organica comprada no CEASA.',
-    punchline: '',
+    punchline: '🤢',
     price: 3.0,
     image: 'beetroot.png'
   },
@@ -58,13 +58,6 @@ const products: Product[] = [
     image: 'cooked-mutton.png'
   },
   {
-    name: 'Cordeiro Assado',
-    description: 'Cordeiro assado.',
-    punchline: 'O fato de ter vindo de uma ovelha é apenas um detalhe.',
-    price: 16.0,
-    image: 'cooked-mutton.png'
-  },
-  {
     name: 'Bifão Frito',
     description: 'Bife de boi feito na chapa.',
     punchline: '"Garçom: Bife de boi..."',
@@ -85,6 +78,77 @@ const products: Product[] = [
       'Definitivamente não utilizado para testes de produto de beleza.',
     price: 14,
     image: 'cooked-rabbit.png'
+  },
+  {
+    name: 'Alga seca',
+    description: 'Alga seca.',
+    punchline: 'É uma alga... que tá seca.',
+    price: 14,
+    image: 'dried-kelp.png'
+  },
+  {
+    name: 'Melancia',
+    description: '94% água.',
+    punchline: 'grep "Piada Melancia:" ../../piadas/piadas.txt',
+    price: 11.2,
+    image: 'melon-slice.png'
+  },
+  {
+    name: 'Bacalhau',
+    description:
+      'Cod is the common name for the demersal fish genus Gadus, belonging to the family Gadidae.[1] Cod is also used as part of the common name for a number of other fish species, and one species that belongs to genus Gadus is commonly not called cod',
+    price: 130.4,
+    image: 'raw-cod.png'
+  },
+  {
+    name: 'Sementes de Cacau',
+    description: 'Sementes de cacau frescas.',
+    punchline: 'Obtidas de forma 100% legal.',
+    price: 60.0,
+    image: 'cocoa-beans.png'
+  },
+  {
+    name: 'Cordeiro Assado',
+    description: 'Cordeiro assado.',
+    punchline: 'O fato de ter vindo de uma ovelha é apenas um detalhe.',
+    price: 16.0,
+    image: 'cooked-mutton.png'
+  },
+  {
+    name: 'Alga seca',
+    description: 'Alga seca.',
+    punchline: 'É uma alga... que tá seca.',
+    price: 14,
+    image: 'dried-kelp.png'
+  },
+  {
+    name: 'Coelho Cozido',
+    description: 'Carne de coelho cozida.',
+    punchline:
+      'Definitivamente não utilizado para testes de produto de beleza.',
+    price: 14,
+    image: 'cooked-rabbit.png'
+  },
+  {
+    name: 'Melancia',
+    description: '94% água.',
+    punchline: 'grep "Piada Melancia:" ../../piadas/piadas.txt',
+    price: 11.2,
+    image: 'melon-slice.png'
+  },
+  {
+    name: 'Bifão Frito',
+    description: 'Bife de boi feito na chapa.',
+    punchline: '"Garçom: Bife de boi..."',
+    price: 7.5,
+    image: 'steak.png'
+  },
+  {
+    name: 'Bifão Frito 2',
+    description: 'Bife de porco feito na chapa.',
+    punchline: '"Garçom: ...ou de porco?"',
+    price: 7.5,
+    image: 'cooked-porkchop.png'
   }
 ];
 
