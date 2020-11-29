82eac826f3e96d6dfa31383b1c8f09cb87e0618a
Kaique Henryque <khenrysodre@gmail.com>
2020-11-29 13:43:36 -0300

    Tela de listagem de Produtos

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,12 +1,21 @@
 import { Container } from '../styles/pages/products.style';
 
 import Navbar from '../components/navbar';
+import Card from '../components/card';
+import products from '../fixtures/products';
 
 const Products: React.FC = () => {
   return (
     <>
       <Navbar />
-      <Container />
+      <Container>
+        {products.map(product => {
+          const { image, price, name } = product;
+          return (
+            <Card key={product.id} image={image} price={price} name={name} />
+          );
+        })}
+      </Container>
     </>
   );
 };
---
+++ src/styles/components/card.style.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+
+export const ProductName = styled.h4`
+  text-align: center;
+  font-size: 1.5rem;
+  font-weight: 550;
+  color: white;
+  width: 100%;
+`;
+
+export const ProductPrice = styled.p`
+  padding: 5px;
+  text-align: center;
+  font-size: 1.2rem;
+  font-weight: 600;
+  color: #8caaf8;
+  width: 100%;
+`;
+
+export const Button = styled.button`
+  border: none;
+  background-color: #33312e;
+  color: #8caaf8;
+  font-size: 1.2rem;
+  font-weight: 500;
+  border-radius: 10px 10px 0px 0px;
+  height: 2rem;
+  width: 10rem;
+`;
+
+export const TextContainer = styled.div`
+  width: 100%;
+  heigth: auto;
+`;
+
+export const Container = styled.div`
+  padding: 15px 15px 0px 15px;
+  border-radius: 5px;
+  text-align: center;
+  align-content: center;
+  width: 100%;
+  height: 100%;
+  background-color: #d1d2f9;
+`;
+
+export const Image = styled.img`
+  height: 250px;
+  width: 250px;
+`;
